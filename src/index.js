78c07101e320e4f857a832248d9863d60102127d
salbyul <kpast96@gmail.com>
2022-12-30 22:16:50 +0900

    add tailwind

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import 'semantic-ui-css/semantic.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 
+=======
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+>>>>>>> b16d7379 (add tailwind)
